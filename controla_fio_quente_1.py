16b1cb559d863567bec93cd06bb37bc7a280d074
Daniel <danielcruztorresgmail.com>
2022-07-01 20:08:13 -0300

    O script está regulando a tensão, porém a leitura de zero-cross está lendo quatro vezes mais do que seria razoàvel

--- controla_fio_quente_1.py
+++ controla_fio_quente_1.py
@@ -1,29 +1,53 @@
 
 import RPi.GPIO as GPIO
-
 import time
 
 
 GPIO.setmode(GPIO.BCM)
 
 input_pin = 4
+output_pin = 26
+
 
 GPIO.setup(input_pin,GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(output_pin,GPIO.OUT)
+GPIO.setwarnings(False)
+
+# apenas para trasnformar em milisegundos
+mili = 0.001 
+
+# delay é o intervalo de tempo de envio da tesão de 5V para o optoaclopador 
+global delay
+delay = 0.9
+delay = delay*mili
+
+
+def pulse(intervalo):
+    GPIO.output(output_pin,GPIO.HIGH)
+    time.sleep(intervalo)
+    GPIO.output(output_pin,GPIO.LOW)
+    print(intervalo,"este é odelay")
+
+    
 
 global cont
 cont = 0
 
-def onRising(ishi):
+def onRising(nonsense):
     global cont
+    global delay
+    pulse(delay)
     cont = cont+1
+    print(cont)
+    
+    
 t_end = time.time()+10
 #while time.time() < t_end:
 GPIO.add_event_detect(input_pin, GPIO.RISING, callback=onRising)
 
  
  
-time.sleep(100
-)
+time.sleep(10)
 GPIO.cleanup()
 print(cont)
 
